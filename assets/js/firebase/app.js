d76ec142e3209c9feaf6e2e9613b1c73424947f3
GuilhermeAml <guilhermesantos871604@gmail.com>
2022-09-21 20:00:25 -0300

    adicionar nova pasta

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,20 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAzFlFTg4wal_k3Ads7Ie6rVe0ybdUr0a4",
+    authDomain: "projeto-semanafront.firebaseapp.com",
+    projectId: "projeto-semanafront",
+    storageBucket: "projeto-semanafront.appspot.com",
+    messagingSenderId: "499632993650",
+    appId: "1:499632993650:web:40a3b32cae7f8e0e4b19eb"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+export default app
